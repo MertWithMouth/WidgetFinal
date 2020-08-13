f5b798e2cdb5ad2fd4c70f358d350b810f583a46
MertWithMouth <34316968+MertWithMouth@users.noreply.github.com>
2020-08-14 01:25:32 +0300

    added some username checks

--- src/App.js
+++ src/App.js
@@ -10,13 +10,32 @@ class App extends Component{
   state = {
     
     username: null,
-    users: []
+    error:''
   }
 
     setUsername = (username) =>{
+      if (username.length >15) {
+        this.setState({error: "Username is too long"})
+    }
+    
+      else if(username.match(/ /g)){
+        this.setState({error: "You cannot use space"})
 
-      this.setState({username})
       }
+
+      else if(username.startsWith("@")){
+        this.setState({error: "Username can't start with special character"})
+
+      }
+
+
+      else{
+        this.setState({username})
+
+      }
+
+     
+  }
 
 
       setUsernameNull=(username)=>{
@@ -31,7 +50,7 @@ class App extends Component{
             
               {
               !this.state.username ?
-                <Login setUsername={this.setUsername} setUsers={this.setUsers}/>
+                <Login setUsername={this.setUsername} setUsers={this.setUsers} error={this.state.error}/>
                 :
                 <MessagePanel className='messagepanel' username = {this.state.username} users={this.state.users} connection={this.connection} setUsernameNull={this.setUsernameNull}/>
               }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,10 @@
-
 import React, { Component } from 'react';
-
 import Podo from '../../Assets/podo.png'
 import './Login.css'
 
-
-
-
 class Login extends Component {
   login=(e)=> {
+
     e.preventDefault();
     this.props.setUsername(e.target.username.value);
   }
@@ -21,19 +17,16 @@ class Login extends Component {
             <div className="imagecontainer">
               <img className="podoimage" src={Podo} alt="podo" />
             </div>
-
             <form className="loginform" onSubmit={this.login}>
-
               <span className="title">Welcome to Jotform Chat</span>
               <div className="logininput">
-
                 <div className="input-group-prepend">
                   <span className="input-group-text">@</span>
                   <input type="text" className="form-control" placeholder="Username" id="usr" name="username" />
+                  
                 </div>
-
+                <div className="errormessage">{this.props.error}</div>
               </div>
-              
               <button className="button"><span>Log-In </span></button>
             </form>
           </div>
